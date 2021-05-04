bad6a582ef27cb653f838389a7ecc3e08543fd03
Mathis <mathis.debiossat38@hotmail.fr>
2021-05-04 16:55:34 +0200

    Fin de séance 1

--- Graphic.py
+++ Graphic.py
@@ -1,6 +1,19 @@
 import core
+import pygame
+import Objet
 
 
-def Init_jeux():
-    #core.WINDOW_SIZE = [850, 800]
-    print("qsds")
+
+class GUI (self):
+
+    core.WINDOW_SIZE = Taille_fenetre
+    Taille_fenetre = [400, 400]
+    Espacement_inter_pions = Taille_pions + 20
+
+
+    def Affich_pos_pions(self):
+        print("jvfhku")
+
+    def Affich_score(self):
+        print("jvfhku")
+
