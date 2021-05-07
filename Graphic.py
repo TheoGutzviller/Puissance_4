02ec6bd510b2d52bf419557705533df361b87c86
Mathis <mathis.debiossat38@hotmail.fr>
2021-05-07 09:49:10 +0200

    hhfgn

--- Graphic.py
+++ Graphic.py
@@ -19,3 +19,30 @@ class GUI:
         for i in range (7):
             for y in range (6):
                 pygame.draw.circle(core.screen, Variable.blanc, ((Variable.Taille_pions+10)+(i*(Variable.Taille_pions*2+10)), (Variable.Taille_pions+10)+(y*(Variable.Taille_pions*2+10))), Variable.Taille_pions)
+
+    def InfoBox(self):
+        clock = pg.time.Clock()
+        input_box1 = InputBox(600, 700, 140, 32)
+        input_box2 = InputBox(100, 700, 140, 32)
+        input_boxes = [input_box1, input_box2]
+        done = False
+
+        while not done:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    done = True
+                for box in input_boxes:
+                    box.handle_event(event)
+
+            for box in input_boxes:
+                box.update()
+
+            screen.fill((30, 30, 30))
+            for box in input_boxes:
+                box.draw(screen)
+
+            pg.display.flip()
+            clock.tick(30)
+
+
+
